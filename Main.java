f659e315f1766b505b0a518a8a7858cc8b59b9b9
YUHEE <sweetyoohee@gmail.com>
2024-07-05 17:13:38 +0900

    Build a First Main : Function_Frint

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+
+	public static void main ( String[] args ) {
+		
+		System.out.println( " Hello Beege " );
+		System.out.println( " Hello My Lovely Cat " );
+		System.out.println( " I want to show the message instead 'Hello world' <3 " );
+	} 
+
+}
